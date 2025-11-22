f133b38755547687d8423eea07626c8587ad3f29
JosMirBonito <josmir2705@hotmail.com>
2025-11-22 02:47:23 -0500

    o

--- src/main/java/com/mycompany/pagoconsola/InventoryManager.java
+++ src/main/java/com/mycompany/pagoconsola/InventoryManager.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class InventoryManager {
-//ads
+//adsd
     private Map<String, Integer> inventory = new HashMap<>();
 
     public void AddItem(String name, int quantity) {
